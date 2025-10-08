492a9993cb5fbb375874694c8eb8996cdfbd0557
digital menu <digitalmenu.global@gmail.com>
2025-10-08 09:40:58 +0530

    added apis for manage the categories

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
--- Insert sample categories
-INSERT INTO categories (id, name, is_visible, display_order, restaurant_id, created_at, updated_at)
-VALUES 
-('cat1', 'Starters', true, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('cat2', 'Main Course', true, 2, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('cat3', 'Desserts', true, 3, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('cat4', 'Beverages', true, 4, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
